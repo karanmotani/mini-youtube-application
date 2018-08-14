05e1c8fa69737f887e602db0cd54bda20249bdb4
Karan Motani <karan.motani94@gmail.com>
2018-08-14 13:28:26 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// When we create a component, we are creating a class of the component (a type)
+// We can have many instances of class (in this case, App)
+// Here, App is a class, not an instance
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+// To render App to the DOM, we need to pass an instance of the class, not the
+// class itself
+// Before rendering, we need to instantiate our components.
+// Creating an instance is very easy
+// It can be done as <App></App> or <App />
+
+ReactDOM.render(<App />, document.querySelector('.container-fluid '));
